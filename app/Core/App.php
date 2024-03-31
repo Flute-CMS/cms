fa8e73ebe2e3bff3ad2ba772926d6a63769fa33d
Nikita <62756604+FlamesONE@users.noreply.github.com>
2024-03-31 21:54:13 +0500

    🧾Update version for release

--- app/Core/App.php
+++ app/Core/App.php
@@ -47,7 +47,7 @@ final class App
      * 
      * @var string
      */
-    public const VERSION = '0.1.0.2-alpha';
+    public const VERSION = '0.1.1-alpha';
 
     /**
      * Set the base path of the application
