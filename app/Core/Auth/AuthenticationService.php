c54046c6eb9efc27b727bde17fdeb0f18d371bfa
Nikita <62756604+FlamesONE@users.noreply.github.com>
2024-04-14 20:28:03 +0500

    fix reset password

--- app/Core/Auth/AuthenticationService.php
+++ app/Core/Auth/AuthenticationService.php
@@ -382,7 +382,7 @@ class AuthenticationService
     {
         $passwordResetTokenRepository = rep(PasswordResetToken::class);
 
-        $passwordResetToken = $passwordResetTokenRepository->findOne(['token' => $token]);
+        $passwordResetToken = $passwordResetTokenRepository->select()->where(['token' => $token])->load('user')->fetchOne();
 
         if ($passwordResetToken === null) {
             throw new PasswordResetTokenNotFoundException($token);
