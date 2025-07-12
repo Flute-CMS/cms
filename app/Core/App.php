524b44829440d19abbd9e5306a2934d18ecb7b31
Nikita <62756604+FlamesONE@users.noreply.github.com>
2025-07-12 22:35:43 +0500

    version change

--- app/Core/App.php
+++ app/Core/App.php
@@ -47,7 +47,7 @@ final class App
     /**
      * @var string
      */
-    public const VERSION = '0.1.6.1';
+    public const VERSION = '0.1.6.2';
 
     /**
      * Set the base path of the application
