873fdb8d67623533ec4e31ecd9fc660b97c95300
crashzk <contato@zkservidores.com>
2026-01-13 23:08:49 -0300

    Update auth.php

--- i18n/br/auth.php
+++ i18n/br/auth.php
@@ -1,6 +1,4 @@
-﻿<?php
-
-return [
+<?php return [
     "confirmation" => [
         "subject"            => "Confirmação de Conta",
         "success"            => "Confirmação realizada com sucesso",
